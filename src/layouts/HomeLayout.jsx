28fb35688d165434478ed96e0532a42f32b3eff2
Adarsh Kumar <adarshkr.227@gmail.com>
2025-01-10 15:32:07 +0530

    Added login signup button to side bar

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,67 +1,114 @@
 import { AiFillCloseCircle } from "react-icons/ai";
 import { FiMenu } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 import Footer from "../components/footer";
 
-const HomeLayout = ({ children }) => {
+function HomeLayout({ children }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+  const role = useSelector((state) => state?.auth?.role);
 
-  function changeWidth(){
+  function changeWidth() {
     const drawerSide = document.getElementsByClassName("drawer-side");
-    drawerSide[0].style.width = 'auto';
+    drawerSide[0].style.width = "auto";
   }
 
-  function hideDrawer(){
+  function hideDrawer() {
     const element = document.getElementsByClassName("drawer-toggle");
     element[0].checked = false;
 
     const drawerSide = document.getElementsByClassName("drawer-side");
-    drawerSide[0].style.width = '0';
+    drawerSide[0].style.width = "0";
   }
 
-
+  function onLogOut(e) {
+    e.preventDefault();
+    // todo
+    navigate("/");
+  }
 
   return (
     <div className="min-h-[90vh] ">
       <div className="drawer absolute left-0 z-50 w-full">
         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+
         <div className="drawer-content">
           <label htmlFor="my-drawer">
-            <FiMenu onClick={changeWidth} size={"32px"} className="font-bold text-white m-4 cursor-pointer" />
+            <FiMenu
+              onClick={changeWidth}
+              size={"32px"}
+              className="font-bold text-white m-4 cursor-pointer"
+            />
           </label>
         </div>
+
         <div className="drawer-side w-0">
           <label
             htmlFor="my-drawer"
-            aria-label="close sidebar"
             className="drawer-overlay"
           ></label>
-          <ul className="menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative">
-              <li className="w-fit absolute right-2 z-50">
-                <button onClick={hideDrawer}>
-                    <AiFillCloseCircle size={24} />
-                </button>
-              </li>
+          <ul className="menu p-4 w-48 h-[100%] sm:w-80 bg-base-200 text-base-content relative">
+            <li className="w-fit absolute right-2 z-50">
+              <button onClick={hideDrawer}>
+                <AiFillCloseCircle size={24} />
+              </button>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            {isLoggedIn && role === "ADMIN" && (
               <li>
-                <Link to='/'>Home</Link>
+                <Link to="/admin/dashboard">Admin Dashboard</Link>
               </li>
-              <li>
-                <Link to='/about'>About us</Link>
+            )}
+            <li>
+              <Link to="/about">About us</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+            <li>
+              <Link to="/courses">All courses</Link>
+            </li>
+
+            {!isLoggedIn ? (
+              <li className="absolute bottom-4 w-[90%]">
+              <div className="w-full flex items-center justify-center">
+                  <button className=" bg-blue-500 hover:bg-blue-700 text-white px-4 py-1 font-semibold rounded-md w-full">
+                      <Link to="/signin">Login</Link>
+                  </button>
+                  <button className=" bg-pink-500 hover:bg-pink-700 text-white px-4 py-1 font-semibold rounded-md w-full">
+                      <Link to="/signup">Signup</Link>
+                  </button>
+              </div>
+          </li>
+            ) : (
+              <li className="absolute bottom-4 w-[90%]">
+                <div className="w-full flex items-center justify-center ">
+                  <button className=" bg-blue-500 hover:bg-blue-700 text-white px-4 py-1 font-semibold rounded-md w-full">
+                    <Link to="/user/profile">Profile</Link>
+                  </button>
+                  <button className=" bg-pink-500 hover:bg-pink-700 text-white px-4 py-1 font-semibold rounded-md w-full">
+                    <Link onClick={onLogOut}>Logout</Link>
+                  </button>
+                </div>
               </li>
-              <li>
-                <Link to='/contact'>Contact Us</Link>
-              </li>
-              <li>
-                <Link to='/courses'>All courses</Link>
-              </li>
+            )}
           </ul>
         </div>
       </div>
+
       {children}
       <Footer />
     </div>
   );
-};
+}
 
 export default HomeLayout;
+
+
+
